78c3fbaeeaa89f7b8bfe71bbc53e6dc844ae20a1
rzzz0 <rudyhsurh@gmail.com>
2023-10-12 20:54:54 +0800

    HIIII

--- src/main/java/com/example/Circle.java
+++ src/main/java/com/example/Circle.java
@@ -2,17 +2,21 @@ package com.example;
 
 public class Circle extends Shape {
 
-    // TODO: Define attributes for the circle
+    private double radius;
 
+    public Circle(double radius) {
+        this.radius=radius;
+    }
     @Override
     public double area() {
         // TODO: Implement the method
-        return 0;
+        
+        return Math.PI * radius * radius;
     }
 
     @Override
     public double perimeter() {
         // TODO: Implement the method
-        return 0;
+        return 2 * Math.PI * radius;
     }
 }
--- src/main/java/com/example/Square.java
+++ src/main/java/com/example/Square.java
@@ -3,16 +3,23 @@ package com.example;
 public class Square extends Shape {
 
     // TODO: Define attributes for the square
+    private int width;
+    
+
+    public Square(int width){
+        this.width = width;
+    }
+    
 
     @Override
     public double area() {
         // TODO: Implement the method
-        return 0;
+        return width * width;
     }
 
     @Override
     public double perimeter() {
         // TODO: Implement the method
-        return 0;
+        return width * 4;
     }
 }
